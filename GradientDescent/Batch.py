ff49390aa4e840f4fb9609d1f6ce938db915bbbf
Edielson P. Frigieri <edielsonpf@gmail.com>
2016-04-02 17:37:23 -0500

    updating batch algorithm adding valid method

--- GradientDescent/Batch.py
+++ GradientDescent/Batch.py
@@ -15,6 +15,12 @@ class BatchClass(object):
         '''
         Constructor
         '''
+    def __Sigmoid(self,x):
+        if x >= 0:
+            return 1
+        else:
+            return -1
+    
     def __RandomlySelect(self,num_observations):
         return np.random.randint(0,num_observations-1,1)
     
@@ -27,44 +33,62 @@ class BatchClass(object):
         
     def train(self,step_lenght,min_threshold,max_epochs,x,y):
             
-            #number of observations: must be disposed by rows
-            numObs = len(x)
-            #number of features: must be disposed by columns
-            numFeat = len(x.T)
-            # counter for the number of epochs
-            numEpochs=0
+        #number of observations: must be disposed by rows
+        numObs = len(x)
+        #number of features: must be disposed by columns
+        numFeat = len(x.T)
+        # counter for the number of epochs
+        numEpochs=0
+        threshold = 1000
+        
+        mu, sigma = 0, 0.1 # mean and standard deviation
+        theta = np.random.normal(mu, sigma, numFeat)
+        
+        lsq=[]
+        lsq.append(self.__leastSquares(theta, x, y))
+        
+        numEpochs=numEpochs+1
+        
+        #each interaction is equivalent to one training epoch
+        while (numEpochs < max_epochs) and (threshold > min_threshold):
+        
+            print('Epoch: %s'%numEpochs)
+            print('Least squares: %g'%lsq[numEpochs-1])
+            #Calculating the gradient of J             
+            gradJ = 0
+            for i in range(numObs):
+                RndObs = self.__RandomlySelect(numObs)
+                gradJ = gradJ + (np.dot(x[RndObs],np.matrix(theta).transpose())-y[RndObs])*x[RndObs]
+            gradJ=gradJ/len(x)
             
+            #Finding the new theta        
+            theta=theta-step_lenght*gradJ
             
-            mu, sigma = 0, 0.1 # mean and standard deviation
-            theta = np.random.normal(mu, sigma, numFeat)
-            
-            lsq=[]
             lsq.append(self.__leastSquares(theta, x, y))
-            
             numEpochs=numEpochs+1
+            threshold = np.sqrt((lsq[numEpochs-1]-lsq[numEpochs-2])**2)
             
-            #each interaction is equivalent to one training epoch
-            while (numEpochs < max_epochs) and (lsq[numEpochs-1] > min_threshold):
-            
-                print('Epoch: %s'%numEpochs)
-                print('Least squares: %g'%lsq[numEpochs-1])
-                #Calculating the gradient of J             
-                gradJ = 0
-                for i in range(numObs):
-                    RndObs = self.__RandomlySelect(numObs)
-                    gradJ = gradJ + (np.dot(x[RndObs],np.matrix(theta).transpose())-y[RndObs])*x[RndObs]
-                gradJ=gradJ/len(x)
+        print('End of training procedure!')
+        if numEpochs >= max_epochs:
+            print('Exceeded the maximum number of epochs: %s' %numEpochs)
+        if threshold <= min_threshold:
+            print('Achieved the desired threshold for the least squares: %s' %threshold)
+        
+        return theta,numEpochs,lsq
+        
+    def valid(self,theta,x,y):
+        #number of observations: must be disposed by rows
+        numObs = len(x)
+        
+        success=0
+        for i in range(numObs):
+                h = np.dot(x[i],np.matrix(theta).transpose())
+                prediction = self.__Sigmoid(h)
                 
-                #Finding the new theta        
-                theta=theta-step_lenght*gradJ
-                
-                lsq.append(self.__leastSquares(theta, x, y))
-                numEpochs=numEpochs+1
-                
-            print('End of training procedure!')
-            if numEpochs >= max_epochs:
-                print('Exceeded the maximum number of epochs: %s' %numEpochs)
-            if lsq[numEpochs-1] <= min_threshold:
-                print('Achieved the desired threshold to the least squares: %s' %numEpochs)
-            
-            return theta,numEpochs,lsq        
+                if  prediction == y[i]:
+                    success = success+1
+        percentage=(1.0*success/numObs)*100
+        errors=numObs-success
+                    
+        return percentage,success,errors
+                    
--- main.py
+++ main.py
@@ -18,8 +18,8 @@ print ('Number of features: %s'%numFeat)
 YTrain = LoadCSV('data/Ytrain.csv', 1)
 
 stepLen = 1.0/32
-minThreshold=0.001
-maxEpoc = 10
+minThreshold=1e-5
+maxEpoc = 30
 
 BachHandle = BatchClass()
 Model,numEpoc, Lsq = BachHandle.train(stepLen, minThreshold, maxEpoc, XTrain, YTrain)
@@ -30,3 +30,17 @@ plt.xlabel('Number of epochs')
 plt.ylabel('Least squares')
 plt.title('Training history')
 plt.show()
+
+
+#Loading data from CSV file
+XTest = LoadCSV('data/Xtest.csv', 50)
+numObs = len(XTest)
+print ('Number of training observations: %s'%numObs) 
+numFeat = len(XTest.T)
+print ('Number of features: %s'%numFeat)
+YTest = LoadCSV('data/Ytest.csv', 1)
+
+percentage,success,errors = BachHandle.valid(Model, XTest, YTest)
+print('Number of observations classified correctly: %g'%success)
+print('Number of observations classified wrongly: %g'%errors)
+print('Percentage correctly classified: %g '%percentage)
